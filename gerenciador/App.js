3efe309d9f5640917fe444a33f9536288c5265e9
Nathan_Santos <nathan.oliver07@hotmail.com>
2026-09-02 20:59:06 -0300

    :books: lógica de login iniciada

--- gerenciador/App.js
+++ gerenciador/App.js
@@ -1,8 +1,22 @@
-import LoginPage from "./src/screens/login.jsx";
+import { useState } from "react";
 
+import LoginPage from "./src/screens/login.jsx";
+import HomePage from "./src/screens/home.jsx";
 
 export default function App() {
-  return (
-      <LoginPage/>      
-  );
+  const [logado, setLogado] = useState(false);
+
+  const irHome = () => {
+    setLogado(true)
+  }
+  const logoutSys = () => {
+    setLogado(false)
+  }
+
+
+  if (logado === true) {
+    return <HomePage onLogout={logoutSys}/>
+  }
+  
+  return <LoginPage onLoginSucess = {irHome}/>;
 }
--- gerenciador/src/screens/home.jsx
+++ gerenciador/src/screens/home.jsx
@@ -1,2 +1,6 @@
 import { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, TextInput } from "react-native";
+export default function HomePage() {
 
+}
