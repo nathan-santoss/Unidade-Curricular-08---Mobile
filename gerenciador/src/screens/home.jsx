f8da70499e354f3e473000e3c4a420cc065e9898
Nathan Santos <nathan.oliver07@hotmail.com>
2026-09-04 09:59:20 -0300

    :books: tela de login iniciada

--- gerenciador/src/screens/home.jsx
+++ gerenciador/src/screens/home.jsx
@@ -1,6 +1,20 @@
 import { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Text, TextInput } from "react-native";
-export default function HomePage() {
+import { Text, TouchableOpacity } from "react-native";
+import { home_css } from "../styles/homeStyles.js";    
 
+export default function HomePage({onLogout}) {
+    <SafeAreaView style = {home_css.container}>
+        <Text style = {home_css.titulo}>
+            <TouchableOpacity 
+                style = {home_css.botaoSair}
+                onPress={onLogout}
+            >
+                <Text style = {home_css.textoBotaoSair}>
+                    Sair
+                </Text>
+            </TouchableOpacity>
+
+        </Text>
+    </SafeAreaView>
 }
